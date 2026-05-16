8b3fed5d979d8219445cfb0ea5ec9df7b5de89c2
Hebe <hebe.azul.usandivaras@mi.unc.edu.ar>
2026-05-16 17:38:17 -0300

    Observer terminado

--- tp_patrones_de_diseno/src/strategy/TransportStrategy.java
+++
@@ -1,8 +0,0 @@
-package strategy;
-
-public interface TransportStrategy {
-    String getName();  //Nombre del transporte
-    double getCost();  //Costo del viaje
-    double getDistance(); //Distancia
-    int getETA();  //Tiempo de arribo
-}
