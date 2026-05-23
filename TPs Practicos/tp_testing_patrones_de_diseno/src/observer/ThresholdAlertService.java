0d651c45223752b03aedf371fb5fbfcd9ccc4870
Franco Leonel Montes <franco.montes@mi.unc.edu.ar>
2026-05-23 20:00:39 -0300

    Refactor AlertObserver with AlertService

--- TPs Practicos/tp_testing_patrones_de_diseno/src/observer/ThresholdAlertService.java
+++ TPs Practicos/tp_testing_patrones_de_diseno/src/observer/ThresholdAlertService.java
@@ -1,5 +1,38 @@
+package observer;
+
+// Alert implementation using thresholds
+public class ThresholdAlertService implements AlertService {
+
+    private final double maxCost;
+    private final int maxEta;
+
+    // Sets max allowed values
+    public ThresholdAlertService(double maxCost, int maxEta) {
+        this.maxCost = maxCost;
+        this.maxEta = maxEta;
+    }
+
+    @Override
+    public boolean shouldAlertCost(double cost) {
+        // Returns true if cost exceeds limit
+        return cost > maxCost;
+    }
+
+    @Override
+    public boolean shouldAlertETA(int eta) {
+        // Returns true if ETA exceeds limit
+        return eta > maxEta;
+    }
+}
+
+
+
+
+
+/*
 // Implementación concreta
+
 public class ThresholdAlertService implements AlertService {
     public ThresholdAlertService(double maxCost, int maxEta) { ... }
 ...
-}
+} */
