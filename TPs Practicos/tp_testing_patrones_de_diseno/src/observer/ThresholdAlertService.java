4ce42d2f309dd042ba3b3b82cdf5d0600f274697
Jesusvalla <jesus211543@gmail.com>
2026-05-23 20:02:46 -0300

    Agregue contenido a la clase ThresHoldAlertService

--- TPs Practicos/tp_testing_patrones_de_diseno/src/observer/ThresholdAlertService.java
+++ TPs Practicos/tp_testing_patrones_de_diseno/src/observer/ThresholdAlertService.java
@@ -1,5 +1,21 @@
-// Implementación concreta
 public class ThresholdAlertService implements AlertService {
-    public ThresholdAlertService(double maxCost, int maxEta) { ... }
-...
+    private final double maxCost;
+    private final int maxEta;
+
+    public ThresholdAlertService(double maxCost, int maxEta) {
+        this.maxCost = maxCost;
+        this.maxEta = maxEta;
+    }
+
+    @Override
+    public boolean shouldAlertCost(double cost) {
+        // Comportamiento definido: Si es igual o mayor al umbral, alerta
+        return cost >= this.maxCost;
+    }
+
+    @Override
+    public boolean shouldAlertETA(int eta) {
+        // Comportamiento definido: Si es igual o mayor al umbral, alerta
+        return eta >= this.maxEta;
+    }
 }
