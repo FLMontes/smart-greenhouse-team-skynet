f5afa9df936ff3929d5c65e6f2a8350ed5e91b62
RamiroBaigorria5 <ramiro.baigorria@mi.unc.edu.ar>
2026-05-26 20:59:17 -0300

    correccion de comentarios

--- TPs Practicos/tp_testing_patrones_de_diseno/src/test/java/logger/FakeLogger.java
+++ TPs Practicos/tp_testing_patrones_de_diseno/src/test/java/logger/FakeLogger.java
@@ -37,31 +37,3 @@ public class FakeLogger implements ILogger {
         mensajesGuardados.clear();
     }
 }
-
-
-/*
-package logger;
-
-import singleton.ILogger;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FakeLogger implements ILogger {
-
-    private List<String> mensajesGuardados = new ArrayList<>();
-
-    @Override
-    public void logWarning(String message) {
-        mensajesGuardados.add(message);
-    }
-
-    public List<String> getMensajesGuardados() {
-        return mensajesGuardados;
-    }
-
-    public void limpiarMensajes() {
-        mensajesGuardados.clear();
-    }
-}
-*/
